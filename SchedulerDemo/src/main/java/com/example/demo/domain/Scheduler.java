b362172fad173ab14703529fdb7e41f1165494d1
sharath <sharath@sharath-Aspire-E5-575>
2019-05-14 09:46:58 +0530

    lombok support

--- SchedulerDemo/src/main/java/com/example/demo/domain/Scheduler.java
+++ SchedulerDemo/src/main/java/com/example/demo/domain/Scheduler.java
@@ -23,30 +23,6 @@ public class Scheduler {
 	private String status;
 	private Timestamp start_date;
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public Timestamp getStart_date() {
-		return start_date;
-	}
-
-	public void setStart_date(Timestamp start_date) {
-		this.start_date = start_date;
-	}
-
 	@Override
 	public String toString() {
 		return "Scheduler [id=" + id + ", status=" + status + ", start_date=" + start_date + "]";
